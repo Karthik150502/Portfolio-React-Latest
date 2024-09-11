12f2841b1d59b627c39f96c6b74eb693aafd6e2b
Karthik150502 <karthikrdycool@gmail.com>
2024-09-11 14:04:19 +0530

    Edited the experience section

--- src/Components/Experience/exp.js
+++ src/Components/Experience/exp.js
@@ -18,17 +18,7 @@ export default function Experience() {
                             <img src={forcececloud} alt="" className='complogo' />
                             <div className='skillbartxt2'>
                                 <h2>Salesforce Developer, 4CE Cloud Labs, Bangalore Nov 2023 – July 2024</h2><br />
-                                <b className='bold'>o Build several reusable LWC component templates for a Mobile Banking Application Project - </b>I facilitated the
-                                team's introduction and developed a range of reusable Lightning Web Components (LWC) templates and UI
-                                elements. These components possess dynamic and configurable properties, significantly diminishing code
-                                redundancy and enhancing performance optimization. <br /><br />
-                                <b className='bold'>o Assisted and built the Integration architecture - </b>An Integration Framework that dynamically incorporates Apex,
-                                Lightning Web Components (LWC), and Salesforce Metadata, implemented for a Mobile Banking Application.
-                                <br /><br />
-                                <b className='bold'>o Configured Business Rule Engine and Omniscripts - </b>
-                                Configured the Business Rule Engine and Omniscript flow for customer onboarding/ Lead Generation optimally for
-                                a Mobile Banking Loan Application process, using the Salesforce Financial Services Cloud, Omniscripts and
-                                Business Rule Engine.
+                                <p>Salesforce Administration, Salesforce Mobile Publisher, Experience Cloud, Apex, Salesforce LWC, Sales Cloud, Financial Services Cloud, Omniscripts</p>
                                 <div className="logos2">
                                     <img src={salesforce} alt="" className='logo2-salesforce' />
                                     <img src="https://www.awesomelwc.com/resources/lwc.png" alt="LWC logo" className='logo2' />
