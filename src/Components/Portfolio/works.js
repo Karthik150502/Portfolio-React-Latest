64de9b4cb3caa6334669429c4894aa8ae39b7f53
Karthik150502 <karthikrdycool@gmail.com>
2024-09-28 08:36:39 +0530

    Latest

--- src/Components/Portfolio/works.js
+++ src/Components/Portfolio/works.js
@@ -13,7 +13,7 @@ import React from 'react'
 export default function works() {
     return (
         <section id="works">
-            <div className="scroller">
+            <div class="scroller">
                 <div class="main">
                     <h1>Projects</h1>
                     <p class="titledisc">Projects that showcase my expertise in some of the modern technolgies.</p>
@@ -27,9 +27,36 @@ export default function works() {
                                 <div class="card_content">
                                     <h2 class="card_title">AI SAAS – Chat with PDF </h2>
                                     <p class="card_text">An AI SaaS application that allows users to study any PDF document by simply asking questions about its content.</p>
-                                    <div className="btns">
+                                    <div class="btns">
                                         <a href="https://github.com/Karthik150502/AI_PDF_Saas" rel="noreferrer" target="_blank" class="btn card_btn">Source</a>
                                         <a href="https://docai-ai-pdf-saas.vercel.app/" rel="noreferrer" target="_blank" class="btn card_btn">Demo</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="cards_item">
+                            <div class="card">
+                                <div class="card_image"><img src={doorbin_video} alt="Doorbin Video Conferencing" /></div>
+                                <div class="card_content">
+                                    <h2 class="card_title">Doorbin - Video Conferencing</h2>
+                                    <p class="card_text">A Video Conferencing Application, used the Stream Video Calling SDK.</p>
+                                </div>
+                                <div class="btns">
+                                    <a href="https://github.com/Karthik150502/Next-Video-Conferencing" rel="noreferrer" target="_blank" class="btn card_btn">Source</a>
+                                    <a href="https://doorbin-video-conferencing.vercel.app/" rel="noreferrer" target="_blank" class="btn card_btn">Demo</a>
+                                </div>
+                            </div>
+                        </li>
+
+                        <li class="cards_item">
+                            <div class="card">
+                                <div class="card_image"><img src={cryptoWallet} alt="Bango - HD Crypto Wallet" /></div>
+                                <div class="card_content">
+                                    <h2 class="card_title">Bango - Crypto Wallet</h2>
+                                    <p class="card_text">A in-momory stored HD Crypto wallet for Solana.</p>
+                                    <div class="btns">
+                                        <a href="https://github.com/Karthik150502/Crypto-Wallet-Bango" rel="noreferrer" target="_blank" class="btn card_btn">Source</a>
+                                        <a href="https://crypto-wallet-bango.vercel.app/" rel="noreferrer" target="_blank" class="btn card_btn">Demo</a>
                                     </div>
                                 </div>
                             </div>
@@ -40,40 +67,14 @@ export default function works() {
                                 <div class="card_content">
                                     <h2 class="card_title">AI Finance and Income Tracking Application</h2>
                                     <p class="card_text">AI powered personal finance tracking application to track expenses. </p>
-                                    <div className="btns">
+                                    <div class="btns">
                                         <a href="https://github.com/Karthik150502/AI-Finance-Tracker-" rel="noreferrer" target="_blank" class="btn card_btn">Source</a>
                                     </div>
                                 </div>
                             </div>
                         </li>
-                        <li class="cards_item">
-                            <div class="card">
-                                <div class="card_image"><img src={cryptoWallet} alt="Bango - HD Crypto Wallet" /></div>
-                                <div class="card_content">
-                                    <h2 class="card_title">Bango - Crypto Wallet</h2>
-                                    <p class="card_text">A in-momory stored HD Crypto wallet for Solana.</p>
-                                    <div className="btns">
-                                        <a href="https://github.com/Karthik150502/Crypto-Wallet-Bango" rel="noreferrer" target="_blank" class="btn card_btn">Source</a>
-                                        <a href="https://crypto-wallet-bango.vercel.app/" rel="noreferrer" target="_blank" class="btn card_btn">Demo</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
 
 
-                        <li class="cards_item">
-                            <div class="card">
-                                <div class="card_image"><img src={doorbin_video} alt="Doorbin Video Conferencing" /></div>
-                                <div class="card_content">
-                                    <h2 class="card_title">Doorbin - Video Conferencing</h2>
-                                    <p class="card_text">A Video COnferencing Application, used the Stream Video Calling SDK.</p>
-                                </div>
-                                <div className="btns">
-                                    <a href="https://github.com/Karthik150502/Next-Video-Conferencing" rel="noreferrer" target="_blank" class="btn card_btn">Source</a>
-                                    <a href="https://doorbin-video-conferencing.vercel.app/" rel="noreferrer" target="_blank" class="btn card_btn">Demo</a>
-                                </div>
-                            </div>
-                        </li>
                     </ul>
                 </div>
             </div>
